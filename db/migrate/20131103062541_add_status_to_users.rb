c5429c80d4c1501a7f9376cf12e263a8356794e2
qwyzmo <qwyzmo@yahoo.com>
2014-01-07 21:30:42 -0800

    trying to fix default value for users.status

--- db/migrate/20131103062541_add_status_to_users.rb
+++ db/migrate/20131103062541_add_status_to_users.rb
@@ -1,6 +1,5 @@
 class AddStatusToUsers < ActiveRecord::Migration
 	def change
-		add_column :users, :status, :integer, 
-			:default => User::STATUS[:pending_email]
+		add_column :users, :status, :integer, :default => User::STATUS[:pending_email]
 	end
 end
