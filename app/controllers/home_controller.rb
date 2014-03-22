7ba29c9c6e74133738f1c14f5a8734c6f04d270d
qwyzmo <qwyzmo@yahoo.com>
2014-03-22 16:21:26 -0700

    refactor css, and more home page styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,11 @@ class HomeController < ApplicationController
 	def index
 		# TODO for now just grab first ten.
 		@qwyz_list = Qwyz.take(20)
+		
+		# TODO get all the authors also.
+		
+		
+		
 		@title = "Home"
 		render 'pages/home'
 	end
