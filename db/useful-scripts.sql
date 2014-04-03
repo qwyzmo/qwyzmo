940328de4731f86d40f4332fbdfe88e45d35122e
qwyzmo <qwyzmo@yahoo.com>
2014-04-03 15:24:58 -0700

    change readme, move images up to top of table cell on votes page

--- db/useful-scripts.sql
+++ db/useful-scripts.sql
@@ -3,3 +3,5 @@ select * from users;
 select * from qwyzs;
 select * from votes;
 select chosen_item_id, count(*) as count from votes where qwyz_id = 2  group by chosen_item_id order by count desc;
+delete from votes;
+select q.id, u.name from users u, qwyzs q where q.user_id = u.id and q.id in (1, 2, 3, 4, 5, 6, 7);
