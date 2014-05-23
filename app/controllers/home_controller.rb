48b4d12e96922a1d2152e55dc7ed8e0a96397d49
qwyzmo <qwyzmo@yahoo.com>
2014-05-22 22:58:31 -0700

    home page: order qwyzs, fix num imgs bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@
 class HomeController < ApplicationController
 	
 	def index
-		# TODO for now just grab first twenty
-		@qwyz_list = Qwyz.take(20)
+		# TODO for now just grab first 50 in descending order by update date.
+		@qwyz_list = Qwyz.limit(50).order(updated_at: :desc)
 		@qwyz_to_author_map = Qwyz.id2author(@qwyz_list)
 		@title = "Home"
 		render 'pages/home'
