b115b01f8e96be822a4c57f9fa41dc06639b4aa1
qwyzmo <qwyzmo@yahoo.com>
2014-05-28 19:25:55 -0700

    added secret creator msg

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,7 @@ class HomeController < ApplicationController
 		@qwyz_list = Qwyz.limit(50).order(updated_at: :desc)
 		@qwyz_to_author_map = Qwyz.id2author(@qwyz_list)
 		@title = "Home"
+		@show_creator = params.key?("showcreator")
 		render 'pages/home'
 	end
 	
