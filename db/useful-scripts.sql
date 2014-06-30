40e9829c9c67e720a4f69d7e04c3f513ed40b595
qwyzmo <qwyzmo@yahoo.com>
2014-06-29 20:12:22 -0700

    fix text on home page

--- db/useful-scripts.sql
+++ db/useful-scripts.sql
@@ -1,7 +1,7 @@
 select * from qwyz_items;
 select * from users;
 select * from qwyzs;
-select * from votes;
+select * from votes where qwyz_id = 5;
 select chosen_item_id, count(*) as count from votes where qwyz_id = 2  group by chosen_item_id order by count desc;
 select q.id, u.name from users u, qwyzs q where q.user_id = u.id and q.id in (1, 2, 3, 4, 5, 6, 7);
 SELECT chosen_item_id, count(*) as vote_count FROM "votes" WHERE "votes"."qwyz_id" = 1 GROUP BY chosen_item_id;
