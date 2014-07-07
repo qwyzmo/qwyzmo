c27dd96c003168a80a243a9a7801c0f58c3df6ee
qwyzmo <qwyzmo@yahoo.com>
2014-07-06 17:54:26 -0700

    another fix, ordering on db stats page

--- app/models/db_stats.rb
+++ app/models/db_stats.rb
@@ -10,10 +10,10 @@ class DbStats
 		# sql = "select count(*), voter_user_id, voter_ip_address " +
 					# "from votes group by voter_ip_address, voter_user_id order by voter_ip_address;"
 		# result = ActiveRecord::Base.connection.execute(sql)
-		Vote.all.group('voter_ip_address').group(:voter_user_id).count
+		Vote.all.order(:voter_ip_address).group(:voter_ip_address).group(:voter_user_id).count
 	end
 	
 	def self.recent_votes_per_ip
-		Vote.where( created_at: 1.day.ago..Time.now ).group('voter_ip_address').group(:voter_user_id).count
+		Vote.where( created_at: 1.day.ago..Time.now ).group(:voter_ip_address).group(:voter_user_id).count
 	end
 end
