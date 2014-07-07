91bf70f57405d1ee985aa4804f2b819671e988e0
qwyzmo <qwyzmo@yahoo.com>
2014-07-06 22:18:37 -0700

    add index to db stats page

--- app/models/db_stats.rb
+++ app/models/db_stats.rb
@@ -6,14 +6,14 @@ class DbStats
 		result.first["num"]
 	end
 	
+	def self.recent_votes_per_ip
+		Vote.where( created_at: 1.day.ago..Time.now ).group(:voter_ip_address).group(:voter_user_id).count
+	end
+
 	def self.votes_per_ip
 		# sql = "select count(*), voter_user_id, voter_ip_address " +
 					# "from votes group by voter_ip_address, voter_user_id order by voter_ip_address;"
 		# result = ActiveRecord::Base.connection.execute(sql)
 		Vote.all.order(:voter_ip_address).group(:voter_ip_address).group(:voter_user_id).count
-	end
-	
-	def self.recent_votes_per_ip
-		Vote.where( created_at: 1.day.ago..Time.now ).group(:voter_ip_address).group(:voter_user_id).count
 	end
 end
